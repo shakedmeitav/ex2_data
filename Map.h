c3cb8a2fac40f7b5090dae6fe40c7bb6aa9b65eb
shaked meitav <shakedmeitav@campus.technion.ac.il>
2018-11-26 10:19:01 +0200

    start

--- Map.h
+++ Map.h
@@ -1,9 +1,31 @@
 #ifndef UNTITLED_MAP_H
 #define UNTITLED_MAP_H
 
-class dictionary{
+class Map{
     void* DS;
-    int * size;
+    int size;
+
+public:
+
+    /**
+ * constractur for the Map
+ * @param name -
+ * @param weapon -
+  * the size will be zero when we will initial
+ */
+    Map(void* DS);
+
+    ~Map();
+
+    Map (const Map &map);
+
+    Map &operator=(const Map &map);
+
+
+
+
+
+
 };
 
 
