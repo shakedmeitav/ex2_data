f8db0bb411b7c3d4e40291d425ab1ad76722cc8d
shaked meitav <shakedmeitav@campus.technion.ac.il>
2018-11-26 12:09:57 +0200

    start

---
+++ Exception.h
@@ -0,0 +1,19 @@
+#ifndef UNTITLED_EXCEPTION_H
+#define UNTITLED_EXCEPTION_H
+
+#include <exception>
+
+namespace dataStructure {
+
+    // dataStructure::exception class
+    class exception : public std::exception {};
+
+    // Specific exceptions. Can be also caught as mtm::exceptions
+    // or std::exceptions
+    class INVALID_INPUT : public dataStructure::exception {};
+    class ERROR_ALLOCATION : public dataStructure::exception {};
+    class FAILURE : public dataStructure::exception {};
+    class SUCCESS : public dataStructure::exception {};
+}
+
+#endif //UNTITLED_EXCEPTION_H
