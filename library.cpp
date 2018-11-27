eb79452af1bfca7f083a02361271da136b46a6a6
shaked meitav <shakedmeitav@campus.technion.ac.il>
2018-11-27 22:32:28 +0200

    start

--- library.cpp
+++ library.cpp
@@ -4,11 +4,31 @@
 #include <iostream>
 #include "Exception.h"
 
-
+/**
+ * the cimplexity is O(1)
+ * @return
+ */
 void *Init(){
     Map* DS= new Map;
     if (DS==NULL){
-
+        return NULL;
     }
     return (void*)DS;
 }
+
+
+
+/**
+ * the cimplexity is O(1)
+ * @return
+ */
+//StatusType Add(void *DS, int key, void* value, void** node){
+//try {
+//    ((Map *) DS)->Add(key, value, node);
+//}
+//catch (dataStructure::INVALID_INPUT& e) {
+//    cerr << "Out of memory";
+//}
+
+//}
+
